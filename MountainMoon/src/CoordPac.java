cd0fc283d47fc38a6f690a27a339780b3b789d30
Jack <jadamelio@gmail.com>
2015-11-28 14:28:07 -0800

    Round two on the basic commit, I am a little rusty

---
+++ MountainMoon/src/CoordPac.java
@@ -0,0 +1,44 @@
+//Class that contains three Cartesian coordinates
+public class CoordPac
+{
+  private int x;
+  private int y;
+  private int z;
+  
+  
+  
+  
+  public CoordPac(int x, int y, int z)
+  {
+    super();
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+  
+  public int getX()
+  {
+    return x;
+  }
+  public void setX(int x)
+  {
+    this.x = x;
+  }
+  public int getY()
+  {
+    return y;
+  }
+  public void setY(int y)
+  {
+    this.y = y;
+  }
+  public int getZ()
+  {
+    return z;
+  }
+  public void setZ(int z)
+  {
+    this.z = z;
+  }
+
+}
---
+++ MountainMoon/src/Rocks.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class Rocks extends Tile
+{
+
+  public Rocks(int index, ArrayList<Integer> adjacentTile, CoordPac[][] nodes,
+      String type, int typeID, ArrayList<Chord> chords,
+      ArrayList<RadialBody> bodies)
+  {
+    super(index, adjacentTile, nodes, type, typeID, chords, bodies);
+    // TODO Auto-generated constructor stub
+  }
+
+}
